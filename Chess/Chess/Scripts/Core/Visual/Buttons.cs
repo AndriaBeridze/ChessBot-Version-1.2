0634da9a948de63f25ba356c829f409fda91c95d
AndriaBeridze <108965989+AndriaBeridze@users.noreply.github.com>
2023-08-13 17:15:11 -0400

    Add files via upload

---
+++ Chess/Chess/Scripts/Core/Visual/Buttons.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using static Chess.Scripts.Data.Variables;
+
+namespace Chess.Scripts.Core.Visual
+{
+      internal class Buttons
+      {
+            static int sizeX = 200, sizeY = 60;
+            public Button exit = new Button()
+            {
+                  Size = new Size(sizeX, sizeY),
+                  BackColor = Color.FromArgb(60, 60, 60),
+                  Location = new Point((boardPaddingX - 30 - sizeX) / 2, boardPaddingY - 30 + backgroundPanelSize - sizeY),
+                  FlatStyle = FlatStyle.Flat,
+                  Text = "Exit",
+                  Font = new Font("Courier New", 20),
+                  ForeColor = Color.FromArgb(200, 200, 200),
+            };
+            public Button playAsWhite = new Button()
+            {
+                  Size = new Size(sizeX, sizeY),
+                  BackColor = Color.FromArgb(60, 60, 60),
+                  Location = new Point(275, 50),
+                  FlatStyle = FlatStyle.Flat,
+                  Text = "Play As White",
+                  Font = new Font("Courier New", 15),
+                  ForeColor = Color.FromArgb(200, 200, 200),
+            };
+            public Button playAsBlack = new Button()
+            {
+                  Size = new Size(sizeX, sizeY),
+                  BackColor = Color.FromArgb(60, 60, 60),
+                  Location = new Point(275, 150),
+                  FlatStyle = FlatStyle.Flat,
+                  Text = "Play As Black",
+                  Font = new Font("Courier New", 15),
+                  ForeColor = Color.FromArgb(200, 200, 200),
+            };
+            public Button playAsRandom = new Button()
+            {
+                  Size = new Size(sizeX, sizeY),
+                  BackColor = Color.FromArgb(60, 60, 60),
+                  Location = new Point(275, 250),
+                  FlatStyle = FlatStyle.Flat,
+                  Text = "Play As Random",
+                  Font = new Font("Courier New", 15),
+                  ForeColor = Color.FromArgb(200, 200, 200),
+            };
+            public Button exit2 = new Button()
+            {
+                  Size = new Size(sizeX, sizeY),
+                  BackColor = Color.FromArgb(60, 60, 60),
+                  Location = new Point(275, 350),
+                  FlatStyle = FlatStyle.Flat,
+                  Text = "Exit",
+                  Font = new Font("Courier New", 20),
+                  ForeColor = Color.FromArgb(200, 200, 200),
+            };
+            public Buttons()
+            {
+                  exit.FlatAppearance.BorderSize = 0;
+                  playAsWhite.FlatAppearance.BorderSize = 0;
+                  playAsBlack.FlatAppearance.BorderSize = 0;
+                  playAsRandom.FlatAppearance.BorderSize = 0;
+                  exit2.FlatAppearance.BorderSize = 0;
+            }
+      }
+}
---
+++ Chess/Chess/Scripts/Data/PieceImages.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using static Chess.Scripts.Data.Pieces;
+using static Chess.Scripts.Data.Variables;
+
+namespace Chess.Scripts.Data
+{
+      internal class PieceImages
+      {
+            public Image[] pieceImage = new Image[24];
+
+            public PieceImages()
+            {
+                  pieceImage[black + rook] = new Bitmap("Pieces/BlackRook.png");
+                  pieceImage[black + queen] = new Bitmap("Pieces/BlackQueen.png");
+                  pieceImage[black + king] = new Bitmap("Pieces/BlackKing.png");
+                  pieceImage[black + knight] = new Bitmap("Pieces/BlackKnight.png");
+                  pieceImage[black + bishop] = new Bitmap("Pieces/BlackBishop.png");
+                  pieceImage[black + pawn] = new Bitmap("Pieces/BlackPawn.png");
+
+                  pieceImage[white + rook] = new Bitmap("Pieces/WhiteRook.png");
+                  pieceImage[white + queen] = new Bitmap("Pieces/WhiteQueen.png");
+                  pieceImage[white + king] = new Bitmap("Pieces/WhiteKing.png");
+                  pieceImage[white + knight] = new Bitmap("Pieces/WhiteKnight.png");
+                  pieceImage[white + bishop] = new Bitmap("Pieces/WhiteBishop.png");
+                  pieceImage[white + pawn] = new Bitmap("Pieces/WhitePawn.png");
+
+                  for (int i = 0; i < 24; i++)
+                  {
+                        if (pieceImage[i] != null)
+                        {
+                              pieceImage[i] = new Bitmap(pieceImage[i], new Size(squareSize, squareSize));
+                        }
+                  }
+            }
+      }
+}
